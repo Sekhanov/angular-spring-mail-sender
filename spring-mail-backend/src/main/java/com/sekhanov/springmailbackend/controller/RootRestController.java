e0f6d6c7fd8bcd4c88a5b4e37ff7ac943c8dcb2f
Sergey Khanov (home) <sekhanov@gmail.com>
2019-08-07 07:28:38 +0300

    module init

---
+++ spring-mail-backend/src/main/java/com/sekhanov/springmailbackend/controller/RootRestController.java
@@ -0,0 +1,18 @@
+package com.sekhanov.springmailbackend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * RootRestController
+ */
+@RestController
+@RequestMapping("/")
+public class RootRestController {
+
+    @GetMapping
+    public String rootMessage() {
+        return "This is root of backend mail";
+    }
+}
