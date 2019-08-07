08fdf3b8d2b90f0591015c5925c4198ce763e996
Sergey Khanov (home) <sekhanov@gmail.com>
2019-08-07 12:18:30 +0300

    wysiwyg

--- angular-mail-frontend/src/app/app.component.ts
+++ angular-mail-frontend/src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { AngularEditorConfig } from '@kolkov/angular-editor';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
-  title = 'angular-mail-frontend';
+export class AppComponent  {
+  name = 'Angular 6';
+  htmlContent = 'sxfsdfsd';
+
+  config: AngularEditorConfig = {
+    editable: true,
+    spellcheck: true,
+    height: '15rem',
+    minHeight: '5rem',
+    placeholder: 'Enter text here...',
+    translate: 'no',
+    toolbarPosition: 'top',
+    defaultFontName: 'Times New Roman',
+    customClasses: [
+      {
+        name: "quote",
+        class: "quote",
+      },
+      {
+        name: 'redText',
+        class: 'redText'
+      },
+      {
+        name: "titleText",
+        class: "titleText",
+        tag: "h1",
+      },
+    ]
+  };
 }
