65924856da9e57a960f64233a582f651bf890e45
s.khanov <sekhanov@gmail.com>
2019-09-05 14:02:27 +0300

    lombok

--- spring-mail-backend/src/main/java/com/sekhanov/springmailbackend/dto/MailMessage.java
+++ spring-mail-backend/src/main/java/com/sekhanov/springmailbackend/dto/MailMessage.java
@@ -4,8 +4,11 @@ import javax.validation.constraints.NotBlank;
 
 import com.sekhanov.springmailbackend.validation.Emails;
 
+import lombok.Getter;
+import lombok.Setter;
 
-
+@Getter
+@Setter
 public class MailMessage {
 
     @NotBlank(message = "field 'subject' cant be blank")
@@ -16,64 +19,5 @@ public class MailMessage {
     @NotBlank(message = "field 'toEmail' cant be blank")
     private String toEmailList;
     @NotBlank(message = "field 'textMessage' cant be blank")
-    private String textMessage;
-    
-
-    /**
-     * @return String return the subject
-     */
-    public String getSubject() {
-        return subject;
-    }
-
-    /**
-     * @param subject the subject to set
-     */
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    /**
-     * @return String return the fromEmail
-     */
-    public String getFromEmail() {
-        return fromEmail;
-    }
-
-    /**
-     * @param fromEmail the fromEmail to set
-     */
-    public void setFromEmail(String fromEmail) {
-        this.fromEmail = fromEmail;
-    }
-
-    /**
-     * @return String return the textMessage
-     */
-    public String getTextMessage() {
-        return textMessage;
-    }
-
-    /**
-     * @param textMessage the textMessage to set
-     */
-    public void setTextMessage(String textMessage) {
-        this.textMessage = textMessage;
-    }
-    
-
-    /**
-     * @return String return the toEmailList
-     */
-    public String getToEmailList() {
-        return toEmailList;
-    }
-
-    /**
-     * @param toEmailList the toEmailList to set
-     */
-    public void setToEmailList(String toEmailList) {
-        this.toEmailList = toEmailList;
-    }
-
+    private String textMessage;  
 }
